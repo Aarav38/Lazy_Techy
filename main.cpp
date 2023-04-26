d3b86bcfd2944b9d4c0194d98e1dc61f655b1174
Aarav <22cs038@charusat.edu.in>
2023-04-26 20:38:34 +0530

    Aarav 's Portion

--- main.cpp
+++ main.cpp
@@ -16,21 +16,3 @@ public:
     }
 };
 
-int main() {
-    int a, b;
-
-    cout << "Enter two positive integers: ";
-    cin >> a >> b;
-
-    MathUtils* math_utils_ptr = new MathUtils(); // create a MathUtils object pointer
-
-    int gcd_value = math_utils_ptr->gcd(a, b); // call gcd method using the pointer
-    int lcm_value = math_utils_ptr->lcm(a, b); // call lcm method using the pointer
-
-    cout << "GCD of " << a << " and " << b << " is " << gcd_value << endl;
-    cout << "LCM of " << a << " and " << b << " is " << lcm_value << endl;
-
-    delete math_utils_ptr; // free the allocated memory
-
-    return 0;
-}
