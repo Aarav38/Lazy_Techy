b37339805ede49ed84c3459435fea04d9b20ae99
Meet 23 <22cs023@charusat.edu.in>
2023-05-03 21:20:14 +0530

    Meet Final Commit

--- main.cpp
+++ main.cpp
@@ -1,39 +1,19 @@
-//LCD and GCD calculator by team LAZY_TECHY
+//LCD and GCD calculator by Team LAZY_TECHY
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
 
-
-class MathUtils {
-public:
-    int gcd(int a, int b) {
-        if (a == 0) {
-            return b;
-        }
-        return gcd(b % a, a);
-    }
-
-    int lcm(int a, int b) {
-        int gcd_value = gcd(a, b);
-        return (a / gcd_value) * b;
-    }
-};
-
-int main() {
+class Calculator {
+private:
     int a, b;
 
-    cout << "Enter two positive integers: ";
-    cin >> a >> b;
+    // function to calculate GCD of two numbers
+    int gcd(int x, int y) {
+        if (y == 0) {
+            return x;
+        }
+        return gcd(y, x % y);
+    }
 
-    MathUtils* math_utils_ptr = new MathUtils(); // create a MathUtils object pointer
-
-    int gcd_value = math_utils_ptr->gcd(a, b); // call gcd method using the pointer
-    int lcm_value = math_utils_ptr->lcm(a, b); // call lcm method using the pointer
-
-    cout << "GCD of " << a << " and " << b << " is " << gcd_value << endl;
-    cout << "LCM of " << a << " and " << b << " is " << lcm_value << endl;
-
-    delete math_utils_ptr; // free the allocated memory
-
-    return 0;
-}
-
+public:
